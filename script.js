7f6463b47c1e50c6cac4201c6c1b8c1c21655e04
Kugo:git config --global user.name Kugogit config --global user.name Kugo <klivickdeda@gmail.com>
2021-06-04 15:18:09 +0300

    dragAndDrop

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-"use strict";
+// Показ "hello world" при нажатие на боксик.
+document.querySelector(".mini_box").onclick = sayHi
 
-let link;
-const id = document.getElementById("sayStart");
-link.onclick = hiWorld;
+function sayHi () {
+   console.log("helloy world")
+};
 
-function hiWorld(){
-   console.log("Привет мир");
-}
+//drag and drop
+// dragStar - начало
+// drahEnd draover drop
+
+const dargAndDrop =  () => {
+   const card = document.querySelectorAll('.js_card');
+   const cellsBox = document.querySelectorAll('.cell-box');
 
 
+   const dragStart = function () {
+      setTimeout( () => {
+         this.classList.add('hide');
+      }, 0)
+   };
+
+   const dragEnd = function () {
+      this.classList.remove('hide');
+   };
+
+   card.addEventListener("dragstart", dragStart);
+   card.addEventListener("dragend", dragEnd);
+
+
+    
+
+
+
+
+
+
+} 
