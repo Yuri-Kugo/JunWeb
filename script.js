859b27ed1205f514583d554fcfd481a36aaf72ad
user <slavad878@gmail.com>
2021-06-05 13:31:40 +0300

    drop on box

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-const dragStart = () => console.log('drag start');
+let draggingCard = undefined;
+
+const dragStart = (card) => () => {
+  draggingCard = card;
+}
+
+const drop = (box) => () => {
+  if (draggingCard) {
+    box.appendChild(draggingCard);
+    draggingCard = undefined;
+  }
+}
+
+const dragOver = (event) => {
+  event.preventDefault();
+}
+
+const dragEnter = (event) => {
+  event.preventDefault();
+}
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+  box.addEventListener('drop', drop(box));
+  box.addEventListener('dragover', dragOver);
+  box.addEventListener('dragenter', dragEnter);
+})
 
 const cards = document.querySelectorAll('.js_card');
 cards.forEach(card => {
-  card.addEventListener('dragstart', dragStart);
+  card.addEventListener('dragstart', dragStart(card));
 })
