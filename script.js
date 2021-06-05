0fd526ad9f03bba3c8da4cb122c348e9bf0e8d1b
user <slavad878@gmail.com>
2021-06-05 13:02:33 +0300

    dnd a box

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// Показ "hello world" при нажатие на боксик.
-document.querySelector(".mini_box").onclick = sayHi
+const dragStart = () => console.log('drag start');
 
-function sayHi () {
-   console.log("helloy world")
-};
-
-//drag and drop
-// dragStar - начало
-// drahEnd draover drop
-
-const dargAndDrop =  () => {
-   const card = document.querySelectorAll('.js_card');
-   const cellsBox = document.querySelectorAll('.cell-box');
-
-
-   const dragStart = function () {
-      setTimeout( () => {
-         this.classList.add('hide');
-      }, 0)
-   };
-
-   const dragEnd = function () {
-      this.classList.remove('hide');
-   };
-
-   card.addEventListener("dragstart", dragStart);
-   card.addEventListener("dragend", dragEnd);
-
-
-    
-
-
-
-
-
-
-} 
+const cards = document.querySelectorAll('.js_card');
+cards.forEach(card => {
+  card.addEventListener('dragstart', dragStart);
+})
